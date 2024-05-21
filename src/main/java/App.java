f3a23d5fff60d874e52571ac48393101f4112dd0
SannaAhmed <s.ahmed@corecomtechacademy.co.uk>
2024-05-21 16:31:26 +0100

    new

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-    }
+        String r = "Rock" ;
+        String p = "Paper" ;
+        String s = "Scissors" ;
+
+        String[] plays = {r, p, s};
+
+        System.out.println("Enter your move");
+
+        int computerMove = random.nextInt(3);
+        ;
 
 
- }
+            }
+        }
+
+
+
+
+
+
